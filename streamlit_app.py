cf1da420125f4304b8b2e758e9d42c5a211b5d7d
Raffaele Mazziotta <raffy@cheeese.ch>
2025-07-19 08:51:06 +0200

    day8-slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from datetime import time
+from datetime import datetime
+
 
 st.header("st.sliders")
 
@@ -8,12 +11,27 @@ st.write(f"**I** am **{age}** old")
 
 st.subheader("Range Slider")
 
-st.slider("blablabla",
+values =st.slider("blablabla",
           0,
           100,
           (20,80)
           )
 
+st.write(values[0])
+st.write(values[1])
 
 
+valuesTime = st.slider("Schedule an appointment,",
+            value=(time(11,30), time(12,45))
+        )
+
+st.write(valuesTime[0])
+st.write(valuesTime[1])
+
+valueDateTime = st.slider("Schedule an appointment",
+                          value=datetime(2025,7,19,8,16)
+                          format="MM//DD//YY - hh:mm"
+)
+
+st.write(valueDateTime)
 
